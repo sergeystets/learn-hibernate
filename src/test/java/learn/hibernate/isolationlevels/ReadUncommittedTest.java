afe7437ac937d4c2b9e9ba170c83c86d50768846
Sergii_Stets <Sergii_Stets@epam.com>
2017-04-10 17:46:26 +0300

    tx isolation tests refactor to see transactions boundaries

--- src/test/java/learn/hibernate/isolationlevels/ReadUncommittedTest.java
+++ src/test/java/learn/hibernate/isolationlevels/ReadUncommittedTest.java
@@ -12,7 +12,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
@@ -32,6 +31,12 @@ import learn.hibernate.entity.User;
 @WebAppConfiguration
 public class ReadUncommittedTest {
 
+    private static final String FIND_ONE = "SELECT * FROM user WHERE user.id = :id";
+    private static final String FIND_ALL_BY_NAME = "SELECT * FROM user WHERE user.name = :name";
+    private static final String UPDATE_NAME = "UPDATE user SET name = :name WHERE id = :id";
+    private static final String INSERT_USER = "INSERT INTO user (name) VALUES (:name)";
+    private static final String INSERT_USER_WITH_ID = "INSERT INTO user (id, name) VALUES (:id, :name)";
+
     private static final RowMapper<User> mapper = BeanPropertyRowMapper.newInstance(User.class);
 
     @Autowired
@@ -45,26 +50,24 @@ public class ReadUncommittedTest {
         TransactionTemplate tx2 = configuredTransactionTemplate();
 
         // preconditions
-        SqlParameterSource insertParams = new BeanPropertySqlParameterSource(new User("Dmytro"));
-        jdbc.update("INSERT INTO user (name) VALUES (:name)", insertParams);
+        jdbc.update(INSERT_USER, new BeanPropertySqlParameterSource(new User("Dmytro")));
 
-        // 1. [tx1] start
-        tx1.execute(s -> {
-            // 2. [tx1] selecting user
-            SqlParameterSource findParams = new MapSqlParameterSource("name", "Dmytro");
-            List<User> usersBefore = jdbc.query("SELECT * FROM user WHERE user.name = :name", findParams, mapper);
+        // ----------------------------------------------tx1------------------------------------------------------------
+        tx1.execute(s1 -> {
+            // [tx1] selecting user
+            List<User> usersBefore = jdbc.query(FIND_ALL_BY_NAME, new MapSqlParameterSource("name", "Dmytro"), mapper);
             assertThat(usersBefore).hasSize(1);
 
-            // 3. [tx2] start
-            // 4. [tx2] doing inserts
-            tx2.execute(s1 -> jdbc.update("INSERT INTO user (name) VALUES (:name)", insertParams));
-            // 4. [tx2] finish
+            // -------------------------------tx2-----------------------------------
+            // [tx2] doing inserts
+            tx2.execute(s2 -> jdbc.update(INSERT_USER, new BeanPropertySqlParameterSource(new User("Dmytro"))));
+            // -------------------------------tx2-----------------------------------
 
-            // 2. [tx1] selecting user (phantom user appeared)
-            List<User> usersAfter = jdbc.query("SELECT * FROM user WHERE user.name = :name", findParams, mapper);
+            // selecting user (phantom user appeared)
+            List<User> usersAfter = jdbc.query(FIND_ALL_BY_NAME, new MapSqlParameterSource("name", "Dmytro"), mapper);
             assertThat(usersAfter).hasSize(2);
-            // 6. [tx1] finish
             return null;
+            // -------------------------------------------tx1-----------------------------------------------------------
         });
     }
 
@@ -73,45 +76,37 @@ public class ReadUncommittedTest {
         TransactionTemplate tx1 = configuredTransactionTemplate();
         TransactionTemplate tx2 = configuredTransactionTemplate();
 
+        User initialUser = new User(2L, "Sergii");
+        User tx1User = new User(2L, "Sergii-TX1");
+        User tx2User = new User(2L, "Sergii-TX2");
+
         // preconditions
-        User initialUser = new User(1L, "Sergii");
-        SqlParameterSource params = new BeanPropertySqlParameterSource(initialUser);
-        jdbc.update("INSERT INTO user (id, name) VALUES (:id, :name)", params);
+        jdbc.update(INSERT_USER_WITH_ID, new BeanPropertySqlParameterSource(initialUser));
 
-        BeanPropertySqlParameterSource findUserParams = new BeanPropertySqlParameterSource(initialUser);
+        // -----------------------------------------------tx1-----------------------------------------------------------
+        tx1.execute(s1 -> {
+            User existing = jdbc.queryForObject(FIND_ONE, new BeanPropertySqlParameterSource(initialUser), mapper);
+            assertThat(existing).isEqualToComparingFieldByField(initialUser);
 
-        // 1. [tx1] start
-        tx1.execute(s -> {
-            User existingUser = jdbc.queryForObject("SELECT * FROM user WHERE user.id = :id", findUserParams, mapper);
-            assertThat(existingUser).isEqualToComparingFieldByField(initialUser);
+            // -------------------------------tx2-----------------------------------
+            // [tx2] update existing user (updates will be lost)
+            tx2.execute(s2 -> jdbc.update(UPDATE_NAME, new BeanPropertySqlParameterSource(tx2User)));
+            // -------------------------------tx2-----------------------------------
 
-            // 2. [tx2] start
-            tx2.execute(s2 -> {
-                        SqlParameterSource updateParams1 = new MapSqlParameterSource().
-                                addValue("id", 1).
-                                addValue("name", "Sergii-TX2");
-
-                        // 3. [tx2] update existing user (updates will be lost)
-                        jdbc.update("UPDATE user SET name = :name WHERE id = :id", updateParams1);
-                        return null;
-                    }
-            );
-            // 4. [tx2] finish
-            SqlParameterSource updateParams2 = new MapSqlParameterSource("name", "Sergii-TX1").addValue("id", 1);
-            // 5. [tx1] update existing user
-            jdbc.update("UPDATE user SET name = :name WHERE id = :id", updateParams2);
+            // [tx1] update existing user
+            jdbc.update(UPDATE_NAME, new BeanPropertySqlParameterSource(tx1User));
             return null;
-        }); // 6. [tx1] finish
+        });
+        // -----------------------------------------------tx1-----------------------------------------------------------
 
-        User actual = jdbc.queryForObject("SELECT * FROM user WHERE user.id = :id", findUserParams, mapper);
-        assertThat(actual).hasFieldOrPropertyWithValue("id", 1L);
-        assertThat(actual).hasFieldOrPropertyWithValue("name", "Sergii-TX1");
+        User actual = jdbc.queryForObject(FIND_ONE, new BeanPropertySqlParameterSource(initialUser), mapper);
+        assertThat(actual).isEqualToComparingFieldByField(tx1User);
     }
 
     private TransactionTemplate configuredTransactionTemplate() {
-        TransactionTemplate tx1 = new TransactionTemplate(txManager);
-        setIsolationAndPropagation(tx1);
-        return tx1;
+        TransactionTemplate transactionTemplate = new TransactionTemplate(txManager);
+        setIsolationAndPropagation(transactionTemplate);
+        return transactionTemplate;
     }
 
     private static void setIsolationAndPropagation(TransactionTemplate t) {
